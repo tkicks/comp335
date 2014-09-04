ccb0567d8a2e70413eb3d12f1504e1ec1a7d8b53
Tyler <tkicks32@gmail.com>
2014-09-04 12:05:22 -0400

    comp335 repo init

---
+++ hw/hw0/tkickham_hw0_1.rkt
@@ -0,0 +1,21 @@
+#lang plai-typed
+
+(define (insertion-sort 
+         [x : (listof number)]) : (listof number)
+  (cond
+    [(empty? x) empty]
+    [else 
+     (cond
+       [(< first first(rest)) (reverse (x))]
+       ;(cons ((first x) number)
+        ;     (insertion-sort (rest x))))
+             
+     )])
+  )
+
+; Tests:
+(test (insertion-sort (list 1 2 3)) (list 1 2 3))
+(test (insertion-sort (list 3 2 1)) (list 1 2 3))
+(test (insertion-sort (list 1 3 1)) (list 1 1 3))
+(test (insertion-sort (list 1)) (list 1))
+(test (insertion-sort empty) empty)
