bb373482eb2b561de37a7047e26dea356103d05d
tkicks <tkicks32@gmail.com>
2014-09-05 14:03:32 -0400

    updated hw

--- hw/hw0/tkickham_hw0_1.rkt
+++ hw/hw0/tkickham_hw0_1.rkt
@@ -4,18 +4,16 @@
          [x : (listof number)]) : (listof number)
   (cond
     [(empty? x) empty]
-    [else 
-     (cond
-<<<<<<< HEAD
-       [(< first first(rest)) (reverse (x))]
-=======
-       [(< first rest[1]) (reverse (x))]
->>>>>>> 3461f577d1edfbd6a7b75ecdc66681a87a74547f
-       ;(cons ((first x) number)
-        ;     (insertion-sort (rest x))))
+    [(< (first x) (first (rest x))) (insertion-sort (rest x))];(cons (first x) (insertion-sort(rest x)))]
+    ;(cons (first x) (insertion-sort(rest x)))]
+    
+    ;good for all tests but sort
+    ;[else (cons (first x) (insertion-sort(rest x)))]
+    ;)
              
-     )])
-  )
+    [else (cons (first x) (insertion-sort (rest x)))])
+    )
+
 
 ; Tests:
 (test (insertion-sort (list 1 2 3)) (list 1 2 3))
