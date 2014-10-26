97f038ecb5c363f430a800aae137cee709099870
tkicks <tkicks32@gmail.com>
2014-10-26 13:06:50 -0400

    finished hw2

--- hw/hw2/tkickham_hw2.rkt
+++ hw/hw2/tkickham_hw2.rkt
@@ -25,11 +25,11 @@
 ; a recursive helper function to find the representation of a function 
 ; definition from the list, given its name
 (define (get-fundef [name : symbol] [fundefs : (listof FunDefC)]) : FunDefC
-  (cond 
+  (cond
     [(empty? fundefs) 
      (error 'get-fundef "function name not found")]
     [(eq? (fdC-name (first fundefs)) name)
-    (first fundefs)]
+     (first fundefs)]
     [else
      (get-fundef name (rest fundefs))]
     ))
@@ -53,8 +53,8 @@
          [else (error 'parse "syntax error")]))]
     [(and (s-exp-list? s) (= 2 (length (s-exp->list s))))
      (let ([sl (s-exp->list s)])
-      (appC (s-exp->symbol (first sl)) (parse (second sl))))]
-     [else (error 'parse "syntax error")]
+       (appC (s-exp->symbol (first sl)) (parse (second sl))))]
+    [else (error 'parse "syntax error")]
     ))
 
 ; a few example test expressions
@@ -62,11 +62,77 @@
 (test (parse '(double 4)) 
       (appC 'double (numC 4)))
 (test (parse '(* (inc5 5) 2)) 
-       (multC (appC 'inc5 (numC 5)) (numC 2)))
+      (multC (appC 'inc5 (numC 5)) (numC 2)))
 (test (parse '(inc10 (+ 2 (+ 1 1)))) 
       (appC 'inc10 (plusC (numC 2) (plusC (numC 1) (numC 1)))))
 (test/exn (parse '(inc5 1 2)) ; too many args
           "syntax error")
 (test/exn (parse '(double)) ; not enough args
           "syntax error")
- 
+
+
+; =========================================================
+; hw2
+
+; binding
+(define-type Binding
+  [bind (name : symbol) (val : number)]
+  )
+
+(define-type-alias Env (listof Binding))
+(define mt-env empty)
+(define extend-env cons)
+
+; interpreter
+(define (interp [expr : ExprC] [env : Env] [fds : (listof FunDefC)]) : number
+  (type-case ExprC expr
+    [numC (n) n]
+    [idC (n) (lookup n env)]
+    [appC (f a)
+          (local ([define fd (get-fundef f fds)])
+            (interp (fdC-body fd)
+                    (extend-env (bind (fdC-arg fd)
+                                      (interp a env fds))
+                                mt-env)
+                    fds))]
+    [plusC (l r) (+ (interp l env fds) (interp r env fds))]
+    [multC (l r) (* (interp l env fds) (interp r env fds))]
+    )
+  )
+
+; lookup
+(define (lookup [for : symbol] [env : Env]) : number
+  (cond
+    [(empty? env) (error 'lookup "name not found")]
+    [else (cond
+            [(symbol=? for (bind-name (first env)))
+             (bind-val (first env))]
+            [else (lookup for (rest env))])]))
+
+
+; tests
+(display "\n================================================\n")
+(display "Tests for interpreter\n\n")
+(test (interp (plusC (numC 10) (appC 'const5 (numC 10)))
+              mt-env
+              (list (fdC 'const5 '_ (numC 5))))
+      15)
+
+(test (interp (plusC (numC 10) (appC 'double (plusC (numC 1) (numC 2))))
+              mt-env
+              (list (fdC 'double 'x (plusC (idC 'x) (idC 'x)))))
+      16)
+
+(test (interp (plusC (numC 10) (appC 'quadruple (plusC (numC 1) (numC 2))))
+              mt-env
+              (list (fdC 'quadruple 'x (appC 'double (appC 'double (idC 'x))))
+                    (fdC 'double 'x (plusC (idC 'x) (idC 'x)))))
+      22)
+
+; test to fix scoping problem
+(display "\n======== Test to fix scoping ============\n")
+(test/exn (interp (appC 'f1 (numC 3))
+                  mt-env
+                  (list (fdC 'f1 'x (appC 'f2 (numC 4)))
+                        (fdC 'f2 'y (plusC (idC 'x) (idC 'y)))))
+          "name not found")
